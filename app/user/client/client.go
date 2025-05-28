7acc04ea597b7fc70f286d3295ad50038d5082a0
WeiXinao <1632967698@qq.com>
2025-05-28 23:32:37 +0800

    feat: 通过直连进行服务发现

--- app/user/client/client.go
+++ app/user/client/client.go
@@ -6,12 +6,13 @@ import (
 
 	v1 "github.com/WeiXinao/daily_your_go/api/user/v1"
 	rpc "github.com/WeiXinao/daily_your_go/gmicro/server/rpcserver"
+	_ "github.com/WeiXinao/daily_your_go/gmicro/server/rpcserver/resolver/direct"
 )
 
 func main() {
 	conn, err := rpc.DialInsecure(
 		context.Background(), 
-		rpc.WithEndpoint("192.168.5.52:8078"),
+		rpc.WithEndpoint("direct:///192.168.5.52:8078"),
 	)
 	if err != nil {
 		panic(err)

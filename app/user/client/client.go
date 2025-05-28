adcd54eed7faf95ad2d9dc873325f61e9f14869b
WeiXinao <1632967698@qq.com>
2025-05-28 18:19:27 +0800

    feat: gmicro rpc 客户端集成 timeout 中间件

--- app/user/client/client.go
+++ app/user/client/client.go
@@ -5,14 +5,13 @@ import (
 	"fmt"
 
 	v1 "github.com/WeiXinao/daily_your_go/api/user/v1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	rpc "github.com/WeiXinao/daily_your_go/gmicro/server/rpcserver"
 )
 
 func main() {
-	conn, err := grpc.NewClient(
-		"127.0.0.1:8078", 
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	conn, err := rpc.DialInsecure(
+		context.Background(), 
+		rpc.WithEndpoint("192.168.5.52:8078"),
 	)
 	if err != nil {
 		panic(err)

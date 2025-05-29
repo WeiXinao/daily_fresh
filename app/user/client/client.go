48afa22c9d4f5c37f8d3e3179cfc5b9b0ec08ba7
WeiXinao <1632967698@qq.com>
2025-05-30 00:04:41 +0800

    feat: gmicro集成负载均衡

--- app/user/client/client.go
+++ app/user/client/client.go
@@ -3,15 +3,22 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	v1 "github.com/WeiXinao/daily_your_go/api/user/v1"
 	"github.com/WeiXinao/daily_your_go/gmicro/registry/consul"
 	rpc "github.com/WeiXinao/daily_your_go/gmicro/server/rpcserver"
 	_ "github.com/WeiXinao/daily_your_go/gmicro/server/rpcserver/resolver/direct"
+	"github.com/WeiXinao/daily_your_go/gmicro/server/rpcserver/selector"
+	"github.com/WeiXinao/daily_your_go/gmicro/server/rpcserver/selector/random"
 	"github.com/hashicorp/consul/api"
 )
 
 func main() {
+	// 设置全局的负载均衡策略
+	selector.SetGlobalSelector(random.NewBuilder())
+	rpc.InitBuilder()
+
 	cfg := api.DefaultConfig()
 	cfg.Address = "192.168.5.52:8500"
 	cfg.Scheme = "http"
@@ -22,7 +29,9 @@ func main() {
 	r := consul.New(cli, consul.WithHealthCheck(true))
 	conn, err := rpc.DialInsecure(
 		context.Background(), 
+		rpc.WithBalacerName("selector"),
 		rpc.WithDiscovery(r),
+		rpc.WithClientTimeout(1 * time.Hour),
 		rpc.WithEndpoint("discovery:///daily-your-go-user-srv"),
 	)
 	if err != nil {
@@ -31,9 +40,13 @@ func main() {
 	defer conn.Close()
 
 	uc := v1.NewUserClient(conn)
-	ulr, err := uc.GetUserList(context.Background(), &v1.PageInfo{})
-	if err != nil {
-		panic(err)
+
+	for {
+		ulr, err := uc.GetUserList(context.Background(), &v1.PageInfo{})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(ulr)
+		time.Sleep(time.Second * 5)
 	}
-	fmt.Println(ulr)
 }

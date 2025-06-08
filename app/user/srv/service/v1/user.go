a8d22d755504f1c412f7d58cdb90b4098d61b809
WeiXinao <1632967698@qq.com>
2025-06-08 15:51:09 +0800

    feat: user 服务 service 层

--- app/user/srv/service/v1/user.go
+++ app/user/srv/service/v1/user.go
@@ -2,12 +2,18 @@ package v1
 
 import (
 	"context"
-	metav1 "github.com/WeiXinao/daily_your_go/pkg/common/meta/v1"
+
 	dv1 "github.com/WeiXinao/daily_your_go/app/user/srv/data/v1"
+	v1 "github.com/WeiXinao/daily_your_go/app/user/srv/data/v1"
+	metav1 "github.com/WeiXinao/daily_your_go/pkg/common/meta/v1"
 )
 
 type UserSrv interface {
-	List(ctx context.Context, opts metav1.ListMeta) (*UserDTOList, error) 
+	List(ctx context.Context, orderBy []string, opts metav1.ListMeta) (*UserDTOList, error)
+	Create(ctx context.Context, user *UserDTO) error
+	Update(ctx context.Context, user *UserDTO) error
+	GetByID(ctx context.Context, id uint64) (*UserDTO, error)
+	GetByMobile(ctx context.Context, mobile string) (*UserDTO, error)
 }
 
 var _ UserSrv = (*userService)(nil)
@@ -16,47 +22,71 @@ type userService struct {
 	userStore dv1.UserStore
 }
 
-func NewUserService(us dv1.UserStore) *userService {
-	return &userService{
-		userStore:	us, 
+// Create implements UserSrv.
+func (u *userService) Create(ctx context.Context, user *UserDTO) error {
+	return u.userStore.Create(ctx, &user.UserDO)
+}
+
+// GetByID implements UserSrv.
+func (u *userService) GetByID(ctx context.Context, id uint64) (*UserDTO, error) {
+	userDO, err := u.userStore.GetByID(ctx, id)
+	if err!= nil {
+		return nil, err
 	}
+	return &UserDTO{*userDO}, nil
 }
 
-type UserDTO struct {
-	Name string
+// GetByMobile implements UserSrv.
+func (u *userService) GetByMobile(ctx context.Context, mobile string) (*UserDTO, error) {
+	udo, err := u.userStore.GetByMobile(ctx, mobile)
+	if err!= nil {
+		return nil, err	
+	}
+	return &UserDTO{
+		UserDO: *udo,
+	}, nil
 }
 
-type UserDTOList struct {
-	TotalCount int64 // 总数
-	Items []*UserDTO  // 数据
+// Update implements UserSrv.
+func (u *userService) Update(ctx context.Context, user *UserDTO) error {
+	return u.userStore.Update(ctx, &user.UserDO)
 }
 
-/* 
-	代码不方便写单元测试用例 
-		1. data 层的接口必须先写好
-		2. 我期望测试的时候测试底层的 data 层的数据按照我期望的返回
-			1. 先手动去插入一些数据
-			2. 去删除一些数据
-		3. 如果 data 层的方法有 bug，坑，我们的代码想要具备好的可测试性
-		
+/*
+代码不方便写单元测试用例
+ 1. data 层的接口必须先写好
+ 2. 我期望测试的时候测试底层的 data 层的数据按照我期望的返回
+ 1. 先手动去插入一些数据
+ 2. 去删除一些数据
+ 3. 如果 data 层的方法有 bug，坑，我们的代码想要具备好的可测试性
 */
-func (u *userService) List(ctx context.Context, opts metav1.ListMeta) (*UserDTOList, error) {
+func (u *userService) List(ctx context.Context, orderBy []string, opts metav1.ListMeta) (*UserDTOList, error) {
 	// 这里是业务逻辑1
-	doList, err := u.userStore.List(ctx, opts)
+	doList, err := u.userStore.List(ctx, orderBy, opts)
 	if err != nil {
 		return nil, err
 	}
 
-	// 业务逻辑2
-	// 代码不方便写单元测试用例
-
 	var userDTOList UserDTOList
 	for _, value := range doList.Items {
-		projectDTO := UserDTO{Name: value.Name}	
+		projectDTO := UserDTO{*value}
 		userDTOList.Items = append(userDTOList.Items, &projectDTO)
 	}
 
-	// 业务逻辑3
-
 	return &userDTOList, nil
+}
+
+func NewUserService(us dv1.UserStore) *userService {
+	return &userService{
+		userStore: us,
+	}
+}
+
+type UserDTO struct {
+	v1.UserDO
+}
+
+type UserDTOList struct {
+	TotalCount int64      // 总数
+	Items      []*UserDTO // 数据
 }
--- app/user/srv/service/v1/user_test.go
+++ app/user/srv/service/v1/user_test.go
@@ -1,14 +1,6 @@
 package v1
 
-import (
-	"context"
-	"testing"
-
-	metav1 "github.com/WeiXinao/daily_your_go/pkg/common/meta/v1"
-	"github.com/WeiXinao/daily_your_go/app/user/srv/data/v1/mock"
-)
-
-func TestUserList(t *testing.T) {
-	userSrv := NewUserService(mock.NewUsers())
-	userSrv.List(context.Background(), metav1.ListMeta{})
-}
+// func TestUserList(t *testing.T) {
+// 	userSrv := NewUserService(mock.NewUsers())
+// 	userSrv.List(context.Background(), metav1.ListMeta{})
+// }
